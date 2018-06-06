c8fc5c4f8aff12a05e512b7645cee1918ef77ea8
LennieCodes <l.pinskiy@yahoo.com>
2018-06-06 11:35:50 -0400

    Added explanation for updateBit function.

--- src/bitManipulation/BitHelper.java
+++ src/bitManipulation/BitHelper.java
@@ -1,6 +1,8 @@
 package bitManipulation;
 
 public class BitHelper {
+    // note that all these operations are 0 based, even if you do them from
+    // the right.
     /*
         This method shifts 1 over by i bits (from right to left), creating
         a value that looks like 00010000. By performing an AND operation 
@@ -36,7 +38,17 @@ public class BitHelper {
         int mask = ~(1 << i);
         return num & mask;
     }
-    
+    /*
+        clearBitsMSBThroughI will clear all bits from the most signiciant to i. It 
+        first creates a number that looks like this (00010000), then subtracts 1 from it,
+        which gives us a number that looks like this: 00011111. If you had a num = 10011111 and you 
+        perform an AND with mask, you get 00011111 which cleared the bits, but it's not inclusive (?) 
+
+        Review this: https://www.helpwithpcs.com/hardware/binary-numbers.php
+        https://stackoverflow.com/questions/12483038/clear-all-bits-from-msb-through-i-inclusive
+
+
+    */
     public static int clearBitsMSBthroughI(int num, int i) {
         int mask = (1 << i) - 1;
         return num & mask;
@@ -47,14 +59,16 @@ public class BitHelper {
         return num & mask;
     }
     /*
-
-
-
+        updateBit updates a bit at location i. To do this, you have to first clear
+        the bit at location i in num. You do this by first creating a mask: 11101111, then AND'ing
+        the mask with num. (Anything AND'd with a 0 is 0.). Then you shift the updateBit in a new
+        value to position i, creating a value that looks like 00000000, and OR that with the cleared num.
+        This will force a bit change. If the value created by (value << i) is 1, it will update to 1, or 0 if 0.
     */
     public static int updateBit(int num, int i, boolean bitIs1) {
         int value = bitIs1 ? 1 : 0;
         int mask = ~(1 << i);
-        return (num & mask) | (value << 1);
+        return (num & mask) | (value << i);
     }
 
 
