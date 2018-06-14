be186ed153c7194e2039b8d1378f2a4d819400ab
LennieCodes <l.pinskiy@yahoo.com>
2018-06-14 08:55:52 -0400

    Added solution for 5.1 (untested, jdk issues on my machine).

--- src/bitManipulation/BitManipulation.java
+++ src/bitManipulation/BitManipulation.java
@@ -2,8 +2,16 @@ package bitManipulation;
 
 public class BitManipulation {
 
-    
+    // 5.1
+    public static int Insertion(int n, int m, int i, int j) {
+        boolean bitIs1 = false;
+        for (int k = 0; i <= j; i++) {
+            bitIs1 = BitHelper.getBit(m, k);
+            n = BitHelper.updateBit(n, i, bitIs1);
+            k++;
+        }
 
-
+        return n;
+    }
 
 }
--- src/bitManipulation/TestRunner.java
+++ src/bitManipulation/TestRunner.java
@@ -4,5 +4,17 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestRunner {
-    
+    @Test
+    public void InsertionTest() {
+        int n = 1024;
+        int m = 10011; 
+        int i = 2;
+        int j = 6;
+
+        int result = BitManipulation.Insertion(n, m, i, j);
+
+        if (result != 1100) {
+            fail("Bit Insertion did not produce expected value of 1100, or 10001001100");
+        }
+    }
 }
